da5239d0f4e3ff2fc942e5d4356914148cbac832
Farshid Mahmoudi <51836304+6arshid@users.noreply.github.com>
2025-05-21 23:19:02 +0200

    Update Create.jsx

--- resources/js/Pages/Posts/Create.jsx
+++ resources/js/Pages/Posts/Create.jsx
@@ -9,14 +9,14 @@ export default function Create() {
     <AuthenticatedLayout
       header={
         <h2 className="text-xl font-semibold leading-tight text-gray-800">
-          {t('create_new_post')}
+          {/* {t('create_new_post')} */}
         </h2>
       }
     >
       <Head title={t('create_new_post')} />
 
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <div className="bg-white shadow-sm rounded-lg p-6">
+        <div className="p-6">
           <div className="max-w-xl mx-auto">
             <h1 className="text-2xl font-bold mb-4">{t('create_a_new_post')}</h1>
             <PostForm />
