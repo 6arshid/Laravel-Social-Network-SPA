292a51526d5674b56cef7136ac91c27f756210bd
Farshid Mahmoudi <51836304+6arshid@users.noreply.github.com>
2025-07-22 00:21:15 +0200

    Update Installed.jsx

--- resources/js/Pages/Installed.jsx
+++ resources/js/Pages/Installed.jsx
@@ -17,6 +17,7 @@ export default function Installed() {
                 <h1 className="text-2xl font-bold mb-4">Installation Complete</h1>
                 <p className="mb-2">Admin email: <strong>admin@admin.com</strong></p>
                 <p>Admin password: <strong>admin@admin.com</strong></p>
+                <p className="mb-6">Please change <strong>your email and password</strong> after logging in.</p>
                 <form onSubmit={removeFile} className="mt-6">
                     <PrimaryButton disabled={processing}>Delete install file</PrimaryButton>
                 </form>
