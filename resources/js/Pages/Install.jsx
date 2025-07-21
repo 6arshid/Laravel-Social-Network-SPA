59c3231cc90754d43afcaf6ea159bba851a03280
Farshid Mahmoudi <51836304+6arshid@users.noreply.github.com>
2025-07-22 00:07:59 +0200

    Update Install.jsx

--- resources/js/Pages/Install.jsx
+++ resources/js/Pages/Install.jsx
@@ -19,6 +19,8 @@ export default function Install() {
         post(route('install.perform'));
     };
 
+    const inputClasses = "mt-1 block w-full bg-black text-white border-gray-600 focus:border-indigo-500 focus:ring-indigo-500";
+
     return (
         <GuestLayout>
             <Head title="Install" />
@@ -26,27 +28,27 @@ export default function Install() {
                 <h2 className="text-xl font-semibold">Database</h2>
                 <div>
                     <InputLabel htmlFor="db_host" value="DB Host" className="text-white" />
-                    <TextInput id="db_host" name="db_host" value={data.db_host} className="mt-1 block w-full" onChange={e => setData('db_host', e.target.value)} required />
+                    <TextInput id="db_host" name="db_host" value={data.db_host} className={inputClasses} onChange={e => setData('db_host', e.target.value)} required />
                     <InputError message={errors.db_host} className="mt-2" />
                 </div>
                 <div>
                     <InputLabel htmlFor="db_port" value="DB Port" className="text-white" />
-                    <TextInput id="db_port" name="db_port" value={data.db_port} className="mt-1 block w-full" onChange={e => setData('db_port', e.target.value)} required />
+                    <TextInput id="db_port" name="db_port" value={data.db_port} className={inputClasses} onChange={e => setData('db_port', e.target.value)} required />
                     <InputError message={errors.db_port} className="mt-2" />
                 </div>
                 <div>
                     <InputLabel htmlFor="db_database" value="DB Database" className="text-white" />
-                    <TextInput id="db_database" name="db_database" value={data.db_database} className="mt-1 block w-full" onChange={e => setData('db_database', e.target.value)} required />
+                    <TextInput id="db_database" name="db_database" value={data.db_database} className={inputClasses} onChange={e => setData('db_database', e.target.value)} required />
                     <InputError message={errors.db_database} className="mt-2" />
                 </div>
                 <div>
                     <InputLabel htmlFor="db_username" value="DB Username" className="text-white" />
-                    <TextInput id="db_username" name="db_username" value={data.db_username} className="mt-1 block w-full" onChange={e => setData('db_username', e.target.value)} required />
+                    <TextInput id="db_username" name="db_username" value={data.db_username} className={inputClasses} onChange={e => setData('db_username', e.target.value)} required />
                     <InputError message={errors.db_username} className="mt-2" />
                 </div>
                 <div>
                     <InputLabel htmlFor="db_password" value="DB Password" className="text-white" />
-                    <TextInput id="db_password" type="password" name="db_password" value={data.db_password} className="mt-1 block w-full" onChange={e => setData('db_password', e.target.value)} />
+                    <TextInput id="db_password" type="password" name="db_password" value={data.db_password} className={inputClasses} onChange={e => setData('db_password', e.target.value)} />
                     <InputError message={errors.db_password} className="mt-2" />
                 </div>
 
