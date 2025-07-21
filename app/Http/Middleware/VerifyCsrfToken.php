9434aa67327da16be63ac9879958b4daae0ce62f
6arshid <51836304+6arshid@users.noreply.github.com>
2025-07-22 00:31:55 +0200

    Fix CSRF path for installer

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
-        '/install', // Exclude install from CSRF check
-        '/install/delete-file', // Allow delete install file without CSRF
+        '*install*', // Allow installer routes regardless of subdirectory
     ];
 }
